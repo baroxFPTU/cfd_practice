e65bcb0480b9bc0398a6c0171d1bba30e2eec892
Barox <75582570+baroxFPTU@users.noreply.github.com>
2021-06-21 23:16:56 +0700

    complete

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,12 +4,28 @@ const main = document.querySelector('.main');
 const body = document.querySelector('body');
 const overlay = document.querySelector('.overlay');
 
+const questions = document.querySelectorAll('.question');
+let duration = 200;
+
 // Events
 btnHamburger.addEventListener('click',() => {
     toggleActiveElms(btnHamburger,nav,main,body, overlay);
 })
 
-overlay.addEventListener('click',() =>     toggleActiveElms(btnHamburger,nav,main,body, overlay))
+overlay.addEventListener('click',() =>toggleActiveElms(btnHamburger,nav,main,body, overlay))
+
+questions.forEach((question) => {
+    question.addEventListener('click',()=> {
+        let activeElm = document.querySelector('.content.active');
+        let content = question.parentElement.querySelector('.content');
+        if (activeElm) {
+            activeElm.classList.remove('active');
+            document.querySelector('.question.active').classList.remove('active');
+        }
+        console.log(content)
+        toggleActiveElms(question,content);
+    })
+})
 
 // Functions
 function toggleActiveElms(...elms) {
@@ -17,3 +33,5 @@ function toggleActiveElms(...elms) {
         elm.classList.toggle('active');
     })
 }
+
+
